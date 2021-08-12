430fc803c34ca7491ddf5cf8af6305ffacf62b09
FDVleuten <86839267+FDVleuten@users.noreply.github.com>
2021-08-12 11:15:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// Function Pizza making
+
+const makePizza = function (){
+  console.log("make the dough");
+  console.log("put the sauce on it");
+  console.log("put the meat and vegies on it");
+  console.log("bake the pizza in the oven");
+  console.log(" ");
+};
+
+const makeSushi = function (){
+  console.log("make the rice");
+  console.log("make the meat and vegies");
+  console.log("put the meat and vegies on the rice");
+  console.log("roll it");
+  console.log(" ");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();
+
